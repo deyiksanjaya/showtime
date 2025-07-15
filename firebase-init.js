8d1ec5ccee16e76bc220b25ade4d880884320066
herisanjaya <153843688+deyiksanjaya@users.noreply.github.com>
2025-07-15 20:49:53 +0800

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,23 @@
+// firebase-init.js
+
+// Impor fungsi-fungsi yang dibutuhkan dari Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
+// Konfigurasi Firebase Anda
+const firebaseConfig = {
+    apiKey: "AIzaSyBm2cw6dircA3IMc3yCEeCgVa5--5W6DUw",
+    authDomain: "showtime-f086c.firebaseapp.com",
+    databaseURL: "https://showtime-f086c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "showtime-f086c",
+    storageBucket: "showtime-f086c.appspot.com",
+    messagingSenderId: "31064800242",
+    appId: "1:31064800242:web:8b290d03eda9bf7d95896b"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Ekspor instance database agar bisa digunakan di file lain
+export { db };
